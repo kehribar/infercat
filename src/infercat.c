3f7f8c48e7c478b4ac2402bebc8cc025bd7e415f
kehribar <ihsan@kehribar.me>
2021-07-24 22:59:20 +0300

    Semi working and _very_ unoptimised conv layer

--- src/infercat.c
+++ src/infercat.c
@@ -4,6 +4,7 @@
 // ----------------------------------------------------------------------------
 #include "infercat.h"
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 
 // ----------------------------------------------------------------------------
@@ -77,6 +78,75 @@ static void infercat_dense(float* input, InfercatLayer_DENSE* ptr)
 }
 
 // ----------------------------------------------------------------------------
+static void infercat_conv2d_single(
+  const float* in, const float* kernel, const float bias,
+  float* out, int32_t iw, int32_t ih, int32_t k, int32_t ch, int32_t offset
+)
+{
+  const int32_t ow = iw - k + 1; // output width
+  const int32_t oh = ih - k + 1; // output height
+  for(int32_t i=0;i<oh;i++)
+  {
+    for(int32_t j=0;j<ow;j++)
+    {
+      for(int32_t y=0;y<k;y++)
+      {
+        for(int32_t x=0;x<k;x++)
+        {
+          const float img = in[j + x + ((i + y) * iw)];
+          const float w = kernel[x + (y * k)];
+          out[offset + (ch * (j + (i * ow)))] += img * w;;
+        }
+      }
+      out[offset + (ch * (j + (i * ow)))] += bias;
+    }
+  }
+}
+
+// ----------------------------------------------------------------------------
+static void infercat_conv2d(float* input, InfercatLayer_CONV2D* ptr)
+{
+  // ...
+  const int32_t iw = ptr->in_width;
+  const int32_t ih = ptr->in_height;
+  const int32_t k = ptr->kernel_width;
+  const int32_t ch = ptr->out_depth;
+
+  // ...
+  float* out = ptr->output_buffer;
+  const int32_t out_size = ptr->out_height * ptr->out_width;
+  memset(out, 0, sizeof(float) * out_size * ptr->out_depth);
+
+  // ...
+  for(int32_t i=0;i<(ptr->out_depth);i++)
+  {
+    // ...
+    const float bias = ptr->bias[i];
+
+    // ...
+    for(int32_t j=0;j<(ptr->in_depth);j++)
+    {
+      // ...
+      const int32_t input_size = ptr->in_width * ptr->in_height;
+      const float* in = &(input[j * input_size]);
+
+      // ...
+      const int32_t kernel_size = ptr->kernel_width * ptr->kernel_width;
+      const float* kernel = &(ptr->weight[i * kernel_size]);
+
+      // ...
+      infercat_conv2d_single(in, kernel, bias, out, iw, ih, k, ch, i);
+    }
+  }
+
+  // ...
+  for(int32_t x=0;x<(ptr->out_depth * out_size);x++)
+  {
+    out[x] = infercat_relu(out[x]);
+  }
+}
+
+// ----------------------------------------------------------------------------
 void infercat_iterate(
   float* input,
   InfercatLayer** ptr,
@@ -95,6 +165,18 @@ void infercat_iterate(
     {
       InfercatLayer_DENSE* layer = (InfercatLayer_DENSE*)(ptr[i]->mem);
       infercat_dense(layer_input, layer);
+      layer_input = layer->output_buffer;
+    }
+    else if(ptr[i]->type == InfercatLayerType_CONV2D)
+    {
+      InfercatLayer_CONV2D* layer = (InfercatLayer_CONV2D*)(ptr[i]->mem);
+      infercat_conv2d(layer_input, layer);
+      layer_input = layer->output_buffer;
+    }
+    else if(ptr[i]->type == InfercatLayerType_MAXPOOLING2D)
+    {
+      InfercatLayer_MAXPOOLING2D* layer = (InfercatLayer_MAXPOOLING2D*)(ptr[i]->mem);
+      // TODO!
       layer_input = layer->output_buffer;
     }
     else
--- src/infercat.h
+++ src/infercat.h
@@ -10,14 +10,17 @@
 
 // ----------------------------------------------------------------------------
 typedef enum{
-  InfercatLayerType_DENSE = 0
+  InfercatLayerType_DENSE = 0,
+  InfercatLayerType_CONV2D = 1,
+  InfercatLayerType_MAXPOOLING2D = 2
 }InfercatLayerType;
 
 // ----------------------------------------------------------------------------
 typedef enum{
   InfercatLayerActivation_RELU = 0,
   InfercatLayerActivation_SOFTMAX = 1,
-  InfercatLayerActivation_SIGMOID = 2
+  InfercatLayerActivation_SIGMOID = 2,
+  InfercatLayerActivation_LINEAR = 3
 }InfercatLayerActivation;
 
 // ----------------------------------------------------------------------------
@@ -30,11 +33,40 @@ typedef struct{
 typedef struct{
   const float* bias;
   const float* weight;
-  float* output_buffer;
   const int32_t input_size;
   const int32_t output_size;
   const InfercatLayerActivation activation;
+  float* output_buffer;
 }InfercatLayer_DENSE;
+
+// ----------------------------------------------------------------------------
+typedef struct{
+  const float* bias;
+  const float* weight;
+  const int32_t stride;
+  const int32_t in_width;
+  const int32_t in_height;
+  const int32_t in_depth;
+  const int32_t out_width;
+  const int32_t out_height;
+  const int32_t out_depth;
+  const int32_t kernel_width;
+  const InfercatLayerActivation activation;
+  float* output_buffer;
+}InfercatLayer_CONV2D;
+
+// ----------------------------------------------------------------------------
+typedef struct{
+  const int32_t stride;
+  const int32_t in_width;
+  const int32_t in_height;
+  const int32_t in_depth;
+  const int32_t out_width;
+  const int32_t out_height;
+  const int32_t out_depth;
+  const int32_t pool_width;
+  float* output_buffer;
+}InfercatLayer_MAXPOOLING2D;
 
 // ----------------------------------------------------------------------------
 void infercat_iterate(
