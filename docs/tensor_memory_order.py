0c77148199c8c0febc15a156518fd1fd090701df
kehribar <ihsan@kehribar.me>
2021-08-07 13:48:07 +0300

    Index check

--- docs/tensor_memory_order.py
+++ docs/tensor_memory_order.py
@@ -29,7 +29,19 @@ print("----")
 print(tensor_img_flat)
 
 # ...
-tensor_kernel = np.zeros((3, 3, 2, 3))
+x = 4
+y = 2
+ch = 1
+print("")
+print("Check")
+print("-----")
+print("%d vs %d" % (
+  tensor_img_flat[(x * (5 * 3)) + (y * 3) + ch],
+  tensor_img[x,y,ch]
+))
+
+# ...
+tensor_kernel = np.zeros((3, 3, 2, 4))
 
 # ...
 for j in range(0, tensor_kernel.shape[3]):
@@ -54,3 +66,16 @@ print("")
 print("FLAT")
 print("----")
 print(tensor_kernel_flat)
+
+# ...
+x = 2
+y = 2
+ch_in = 1
+ch_out = 3
+print("")
+print("Check")
+print("-----")
+print("%d vs %d" % (
+  tensor_kernel_flat[(x * (3 * 4 * 2)) + (y * 4 * 2) + (ch_in * 4) + ch_out],
+  tensor_kernel[x,y,ch_in,ch_out]
+))
