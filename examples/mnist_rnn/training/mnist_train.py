adb88e476a1cf930955adb14dc7aaed6685622b6
kehribar <ihsan@kehribar.me>
2021-08-08 11:42:25 +0300

    GRU based model

--- examples/mnist_rnn/training/mnist_train.py
+++ examples/mnist_rnn/training/mnist_train.py
@@ -15,10 +15,6 @@ print("tensorflow version:", tf.version.VERSION)
 # The data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-# Make sure images have shape (28, 28, 1)
-x_train = np.expand_dims(x_train, -1)
-x_test = np.expand_dims(x_test, -1)
-
 # Scale images to the [0, 1] range
 x_train = x_train.astype("float32") / 255.0
 x_test = x_test.astype("float32") / 255.0
@@ -33,14 +29,10 @@ y_train = tf.keras.utils.to_categorical(y_train, 10)
 y_test = tf.keras.utils.to_categorical(y_test, 10)
 
 # ...
-model = tf.keras.models.Sequential([
-  tf.keras.layers.InputLayer((28, 28, 1)),
-  tf.keras.layers.Conv2D(5, kernel_size=(3, 3), strides=(1, 1), activation="relu", use_bias=True),
-  tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
-  tf.keras.layers.Conv2D(3, kernel_size=(3, 3), strides=(1, 1), activation="relu", use_bias=True),
-  tf.keras.layers.Conv2D(3, kernel_size=(3, 3), strides=(2, 2), activation="relu", use_bias=True),
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(10, activation="softmax"),
+model = tf.keras.Sequential([
+  tf.keras.layers.InputLayer(input_shape=(28, 28)),
+  tf.keras.layers.GRU(128, reset_after=False, activation='relu', recurrent_activation='sigmoid', use_bias=True),
+  tf.keras.layers.Dense(10, activation='softmax', use_bias=True)
 ])
 
 # ...
@@ -54,8 +46,8 @@ model.compile(
 print("")
 model.fit(
   x_train, y_train, 
-  batch_size=512, 
-  epochs=8
+  batch_size=128, 
+  epochs=2
 )
 
 # ...
